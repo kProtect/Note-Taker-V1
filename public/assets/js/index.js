9a89c369cccf7631cdc93ef695929ac2edf177fa
unknown <kyujin0704@gmail.com>
2022-08-13 21:10:01 -0400

    Procfile

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -77,7 +77,9 @@ const handleNoteSave = () => {
   });
 };
 
+// Delete the clicked note
 const handleNoteDelete = (e) => {
+  // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
 
   const note = e.target;
@@ -93,12 +95,14 @@ const handleNoteDelete = (e) => {
   });
 };
 
+// Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
+// Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
@@ -112,6 +116,7 @@ const handleRenderSaveBtn = () => {
   }
 };
 
+// Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const apiRoutes = require('../Develop/routes/apiRoutes');
-const htmlRoutes = require('../Develop/routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
